ee67f60d1c43c255be82ec9257bb410094d3a098
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-02-22 22:17:38 -0400

    add port information

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,5 +2,5 @@ import envConfig from './config/config';
 import app from './app';
 
 app.listen(envConfig.PORT, () => {
-	console.log(`Server running in port ${envConfig.HOST}`);
+	console.log(`Server running in port ${envConfig.HOST}:${envConfig.PORT}`);
 });
