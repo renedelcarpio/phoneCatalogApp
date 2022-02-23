d75731a04258d3a028d552749ec3143f9b6e4a8f
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-02-22 22:19:22 -0400

    controller configuration

---
+++ Backend/src/controllers/phoneController.js
@@ -0,0 +1,29 @@
+import HTTPCodes from '../constants/httpCodes';
+import mockedPhones from '../db/mockedPhones';
+
+const getAll = (req, res) => {
+	const phones = mockedPhones;
+	try {
+		res.status(HTTPCodes.Success).json(phones);
+	} catch (error) {
+		console.log(error);
+		res.status(HTTPCodes.BadRequest).json({ error });
+	}
+};
+
+const find = (req, res) => {
+	try {
+		const phoneId = req.params.id;
+		const phones = mockedPhones;
+		const findPhone = (id) => {
+			return phones.filter((phone) => phone.id == id);
+		};
+
+		res.status(HTTPCodes.Success).json(findPhone(phoneId));
+	} catch (error) {
+		console.log(error);
+		res.status(HTTPCodes.BadRequest).json({ error });
+	}
+};
+
+export { getAll, find };
