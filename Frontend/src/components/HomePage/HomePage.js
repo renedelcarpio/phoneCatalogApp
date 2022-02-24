37c9b67101e2b5ae85b1fd13bf995b378f3eb48c
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-02-24 03:15:26 -0400

    create HomePage component

--- Frontend/src/components/HomePage/HomePage.js
+++ Frontend/src/components/HomePage/HomePage.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setPhones } from '../../redux/actions/phonesAction';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import PhoneCard from '../PhoneCard/PhoneCard';
+import './HomePage.scss';
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const [phone, setPhone] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const getData = async () => {
+		const { data } = await axios.get('http://localhost:5000/phones');
+		setPhone(data);
+		dispatch(setPhones(data));
+	};
+
+	const handleClick = (id) => {
+		history.push(`/${id}`);
+	};
+
+	return (
+		<div className='main__container'>
+			<h1 className='title'>Phone Catalog App</h1>
+			<div className='phones__container'>
+				{phone.map(({ id, brand, model, price, img }) => {
+					return (
+						<PhoneCard
+							key={id}
+							brand={brand}
+							model={model}
+							price={price}
+							img={img}
+							onClick={() => handleClick(id)}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
